ff42a9bc7737eedab3ac1792c15d688910b10365
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-07 01:25:14 +0530

    Add MainSceneController

--- jdbc/02-jdbc-app1/src/main/java/lk/ijse/dep13/jdbc/app/controller/MainSceneController.java
+++ jdbc/02-jdbc-app1/src/main/java/lk/ijse/dep13/jdbc/app/controller/MainSceneController.java
@@ -1,4 +1,27 @@
 package lk.ijse.dep13.jdbc.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public TableView tblCustomers;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public Button btnSave;
+    public Button btnDelete;
+    public Button btnNewCustomer;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
